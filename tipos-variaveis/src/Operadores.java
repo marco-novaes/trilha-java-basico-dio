6ad09b698395b8c907a47040e0b877eac6db721f
Marco Novaes <marco-novaes@uol.com.br>
2022-08-16 10:24:07 -0300

    Aula 7 Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,32 +1,19 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        String nomeUm = "MARCO";
-        String nomeDois = "MARCO";
+     boolean condicao1 = true;
 
-        System.out.println(nomeUm.equals(nomeDois));
+     boolean condicao2 = true;
+ 
+     if(condicao1 && (7 > 4)){
+        System.out.println("as duas condições são verdadeiras");
+     }
 
+     if(condicao1 || condicao2){
+        System.out.println("Uma das condições é verdadeira");
+     }
 
-       int numero1 = 1;
-       int numero2 = 2;
-
-       boolean simNao = numero1 == numero2;
-
-       if(numero1 == numero2){
-        System.out.println("a nossa condição é verdadeira");
-       }
-
-       System.out.println("numeroUm é igual a numeroDois? " +simNao);
-
-       simNao = numero1 != numero2;
-
-       System.out.println("numeroUm é diferente a numeroDois? " +simNao);
-
-        simNao = numero1 > numero2;
-
-       System.out.println("numeroUm é maior a numeroDois? " +simNao);
-        
- }
-
+     System.out.println("fim");
+    }
    
 }
