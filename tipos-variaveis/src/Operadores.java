529a5ffcf9bfba212bf9f25fa0be3bea2443b25b
Marco Novaes <marco-novaes@uol.com.br>
2022-08-16 10:08:09 -0300

    Aula 4 Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,14 +1,22 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        int numero = 5;
+    //     int numero = 5;
 
-        numero = -numero;
+    //     //x repeticao
+        
+    //     //numero = numero + 1;
 
-        System.out.println(numero);
+    //     System.out.println(numero --);
 
-        numero = numero * -1;
+    //     System.out.println(numero);
 
-        System.out.println(numero);
-    }
+    boolean  variavel = true;
+
+    variavel = !variavel;
+
+    System.out.println(variavel);
+ }
+
+   
 }
