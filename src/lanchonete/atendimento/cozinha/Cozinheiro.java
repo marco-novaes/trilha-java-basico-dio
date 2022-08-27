708ba81b4f3cfafc44383ffb5bc7bf06ccd20fd2
Marco Novaes <marco-novaes@uol.com.br>
2022-08-27 09:22:53 -0300

    Exemplo Pacotes

---
+++ src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -0,0 +1,59 @@
+package lanchonete.atendimento.cozinha;
+
+public class Cozinheiro {
+
+	public void adicionarLancheNoBalcao() {
+		System.out.println("ADICIONANDO LANCHE NATURAL HAMBURGER NO BALCAO");
+	}
+	
+	public void adicionarSucoNoBalcao() {
+		System.out.println("ADICIONANDO SUCO NO BALCAO");
+	}
+	
+	public void adicionarComboNoBalcao() {
+		adicionarLancheNoBalcao();
+		adicionarSucoNoBalcao();
+	}
+	
+	private void prepararLanche() {
+		System.out.println("PREPARANDO LANCHE TIPO HAMBURGER");
+	}
+	
+	private void prepararVitamina() {
+		System.out.println("PREPARANDO VITAMINA");
+	}
+	
+	private void prepararCombo() {
+		prepararLanche();
+		prepararVitamina();
+	}
+	public void selecionarIngredientesLanche() {
+		System.out.println("SELECIONADO O PÃO, SALADA, OVO E CARNE");
+	}
+	
+	private void selecionarIngredientesVitamina() {
+		System.out.println("SELECIONADO FRUTA, LEITE E SUCO");
+	}
+	
+	private void lavarIngredientes() {
+		System.out.println("LAVANDO INGREDIENTES");
+	}
+	
+	private void baterVitaminaLiquidificador() {
+		System.out.println("BATENDO VITAMINA LIQUIDIFICADOR");
+	}
+	
+	private void fritarIngredientesLanche() {
+		System.out.println("FRITANDO A CARNE E OVO PARA O HAMBURGER");
+	}
+	
+	
+	
+	private void pedirParaTrocarGas(Almoxarife meuAmigo) {
+		meuAmigo.trocarGas();
+	}
+	
+	private void pedirIngredientes(Almoxarife almoxarife) {
+		almoxarife.entregarIngredientes();
+	}
+}
